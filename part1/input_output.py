014c50babad2cf29a6fca9c6a7dc4daf751e120c
rawzes <rawzes@gmail.com>
2022-08-30 15:44:55 +0300

    Completed lesson 2.3

--- part1/input_output.py
+++ part1/input_output.py
@@ -18,3 +18,21 @@ print('*******')
 
 for i in range(7):
     print('*' * i)
+
+
+print('a', 'b', 'c', sep='***')
+print('a', 'b', 'c', end='\t')
+print('d')
+
+print('I', 'like', 'python', sep='***')
+
+'''
+separator = input()
+str1 = input()
+str2 = input()
+str3 = input()
+print(str1, str2, str3, sep=separator)
+'''
+
+name = input()
+print("Привет,", name, end='!')
---
+++ part1/variables.py
@@ -0,0 +1,15 @@
+a, b = 'Jhon', 'Snow'
+
+print(a, b)
+print('name:', a, 'surname:', b)
+
+a, b = input(), input()
+print('name: ', a, 'surname: ', b)
+
+
+# change values
+name1 = 'Timur'
+name2 = 'Gvido'
+print(name1, name2)
+name1, name2 = name2, name1
+print(name1, name2)
