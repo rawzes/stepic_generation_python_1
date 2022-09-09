756958596cbe400b23f905d20135fea312cf1400
rawzes <rawzes@gmail.com>
2022-09-09 08:32:08 +0300

    completed 2.5

--- part1/step_2_5_ariphmetics.py
+++ part1/step_2_5_ariphmetics.py
@@ -1,13 +1,24 @@
+
 n = int(input())
-c = n % 10
-b = (n // 10) % 10
-a = n // 100
-print(a, b, c, sep='')
-print(a, c, b, sep='')
-print(b, a, c, sep='')
-print(b, c, a, sep='')
-print(c, a, b, sep='')
-print(c, b, a, sep='')
+d = n % 10
+c = n % 100 // 10
+b = n // 100 % 10
+a = n // 1000
+print('Цифра в позиции тысяч равна', a)
+print('Цифра в позиции сотен равна', b)
+print('Цифра в позиции десятков равна', c)
+print('Цифра в позиции единиц равна', d)
+
+# n = int(input())
+# c = n % 10
+# b = (n // 10) % 10
+# a = n // 100
+# print(a, b, c, sep='')
+# print(a, c, b, sep='')
+# print(b, a, c, sep='')
+# print(b, c, a, sep='')
+# print(c, a, b, sep='')
+# print(c, b, a, sep='')
 
 
 # n = int(input())
