30695b54eb042e643993c012cc8778fcc0a60fa5
rawzes <rawzes@gmail.com>
2022-09-09 08:27:05 +0300

    completed 2.4

---
+++ part1/step_2_4_ariphmetics.py
@@ -0,0 +1,45 @@
+'''a = int(input())
+b = int(input())
+
+result = 3*(a+b)*(a+b)*(a+b) + 275*b*b - 127*a - 41
+print(result)
+'''
+
+#  Следующее за числом 20 число: 21
+#  Для числа 20 предыдущее число: 19
+'''
+number = int(input())
+print('Следующее за числом', number, 'число:', number+1)
+print('Для числа', number, 'предыдущее число:', number-1)
+'''
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(3*(a+b+c+d))
+'''
+'''
+a = int(input())
+b = int(input())
+
+print(a, '+', b, '=', a+b)
+print(a, '-', b, '=', a-b)
+print(a, '*', b, '=', a*b)
+'''
+'''
+a = int(input())
+d = int(input())
+n = int(input())
+
+print(a + d*(n-1))
+'''
+'''
+x = int(input())
+print(x, 2*x, 3*x, 4*x, 5*x, sep='---')
+'''
+a = 15 // (16 % 7)
+print(a)
+b = 34 % a * 5 - 29 % 5 * 2
+print(a + b)
---
+++ part1/step_2_5_ariphmetics.py
@@ -0,0 +1,48 @@
+n = int(input())
+c = n % 10
+b = (n // 10) % 10
+a = n // 100
+print(a, b, c, sep='')
+print(a, c, b, sep='')
+print(b, a, c, sep='')
+print(b, c, a, sep='')
+print(c, a, b, sep='')
+print(c, b, a, sep='')
+
+
+# n = int(input())
+# a = n % 10
+# b = (n // 10) % 10
+# c = n // 100
+# print("Сумма цифр =", a+b+c)
+# print('Произведение цифр =', a*b*c)
+
+# min = int(input())
+# hours = min // 60
+# print(min, 'мин - это', hours, 'час', min % 60, 'минут.')
+
+# n = int(input())
+# b = -n//4
+# print(-b)
+
+
+# n = int(input())
+# print(n//2 + n % 2)
+
+
+# n = int(input())
+# k = int(input())
+# print(k // n)
+# print(k % n)
+
+
+# s = int(input())
+# print(s // 100)
+
+'''
+b = int(input())
+q = int(input())
+n = int(input())
+
+print(b*q**(n-1))
+'''
