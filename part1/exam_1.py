fd5af2f698dbfdb5920f869d1cf9f20c6f743063
rawzes <rawzes@gmail.com>
2022-09-09 11:32:57 +0300

    Completed exam_1

---
+++ part1/exam_1.py
@@ -0,0 +1,34 @@
+# a = int(input())
+# b = int(str(a) * 2)
+# c = int(str(a) * 3)
+#
+# print(a + b + c)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+#
+# print(a ** b + c ** d)
+
+# a = int(input())
+# b = int(input())
+# print('Квадрат суммы', a, 'и', b, 'равен', (a+b)**2)
+# print('Сумма квадратов', a, 'и', b, 'равна', a*a + b*b)
+
+'''
+*****************
+*               *
+*               *
+*****************
+
+'''
+
+# print('*' * 17)
+# print('*', '*', sep=' ' * 15)
+# print('*', '*', sep=' ' * 15)
+# print('*' * 17)
+
+# a = 17 // (23 % 7)
+# b = 34 % a * 5 - 29 % 4 * 3
+# print(a * b)
---
+++ part1/numbers_2_3.py
@@ -0,0 +1,17 @@
+'''
+x = int(input())
+print(x)
+print(x+1)
+print(x+2)
+'''
+
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+print(a+b+c)
+'''
+
+a = int(input())
+print('Объем =', a*a*a)
+print('Площадь полной поверхности =', 6*a*a)
