11f2b4dc48983ea167517118de5ff0795e754549
rawzes <rawzes@gmail.com>
2022-09-13 12:23:05 +0300

    Working on lesson 6.1

---
+++ part6/lesson_6_1_step3.py
@@ -0,0 +1,16 @@
+n = float(input())
+
+if n != 0:
+    print(1 / n)
+else:
+    print('Обратного числа не существует')
+
+# s, v1, v2 = float(input()), float(input()), float(input())
+#
+# print(s / (v1 + v2))
+
+
+#
+# a, b = float(input()), float(input())
+#
+# print(0.5 * a * b)
