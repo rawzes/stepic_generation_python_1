8200f02bcca62dc7a8e084bcca1a5da72dd6d8f2
Raman Razumeika <rawzes@gmail.com>
2022-09-28 22:44:58 +0300

    completed lesson 6_2 step 4

---
+++ part6/lesson_6_3_step3.py
@@ -0,0 +1,7 @@
+
+from math import *
+
+r = float(input())
+
+print(pi * r ** 2)
+print(2 * pi * r)
---
+++ part6/lesson_6_3_step4.py
@@ -0,0 +1,8 @@
+import math
+
+a, b = float(input()), float(input())
+
+print((a + b) / 2)
+print(math.sqrt(a * b))
+print(2 * a * b / (a + b))
+print(math.sqrt((math.pow(a, 2) + math.pow(b, 2)) / 2))
