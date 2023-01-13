362dd13f7b96399d4667941034a2da185713dc32
rawzes <rawzes@gmail.com>
2023-01-13 16:24:22 +0300

    completed lesson 13_1

---
+++ part13/l_13_1/l_13_1_step8.py
@@ -0,0 +1,9 @@
+def draw_box():
+    print('*' * 10)
+    for _ in range(12):
+        print('*' + ' ' * 8 + '*', sep='')
+    print('*' * 10)
+
+
+draw_box()
+
---
+++ part13/l_13_1/l_13_1_step9.py
@@ -0,0 +1,7 @@
+def draw_triangle():
+    size = 10
+    for i in range(1, size + 1):
+        print('*' * i)
+
+
+draw_triangle()
